a5af25709f44858a12194a06286f7d7bdfd92f64
Eugene <e@egeek.me>
2025-01-17 18:11:01 +0200

    Build rockbox for Sansa Clip+

--- rockbox-git/create-builds.sh
+++ rockbox-git/create-builds.sh
@@ -2,22 +2,7 @@
 cd /tmp
 mkdir -p /usr/src/rockbox/artifacts
 
-# FullZip for iPod Classic 4G Black & White
-/usr/src/rockbox/rockbox/tools/configure --target=24 --type=N
+# FullZip for Sansa Clip+
+[[ -f Makefile ]] || /usr/src/rockbox/rockbox/tools/configure --target=62 --type=N
 make clean && make -j && make fullzip
-mv rockbox-full.zip /usr/src/rockbox/artifacts/ipodclassic4gbw-rockbox-full.zip
-
-# FullZip for iPod Classic 5G (Video)
-/usr/src/rockbox/rockbox/tools/configure --target=22 --type=N
-make clean && make -j && make fullzip
-mv rockbox-full.zip /usr/src/rockbox/artifacts/ipodclassic5g-rockbox-full.zip
-
-# FullZip for iPod Mini 1G
-#/usr/src/rockbox/rockbox/tools/configure --target=25 --type=N
-#make clean && make -j && make fullzip
-#mv rockbox-full.zip /usr/src/rockbox/artifacts/ipodmini1g-rockbox-full.zip
-
-# FullZip for iPod Mini 2G
-/usr/src/rockbox/rockbox/tools/configure --target=26 --type=N
-make clean && make -j && make fullzip
-mv rockbox-full.zip /usr/src/rockbox/artifacts/ipodmini2g-rockbox-full.zip
+mv rockbox-full.zip /usr/src/rockbox/artifacts/sansaclipp-rockbox-full.zip
