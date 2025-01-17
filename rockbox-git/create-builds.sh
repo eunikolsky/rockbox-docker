2912093beb1d1cfff3e1cec33a4fca36c5f9ede0
Eugene <e@egeek.me>
2025-01-17 20:23:53 +0200

    Make the builds repeatable
    This updates the build script to use the build directory on the host
    (`rockbox-git/build/`) and not to clean it on every run. This change
    makes patching and rebuilding rockbox much faster.

--- rockbox-git/create-builds.sh
+++ rockbox-git/create-builds.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eu
 
-cd /tmp
-mkdir -p /usr/src/rockbox/artifacts
+mkdir -p /usr/src/rockbox/{artifacts,build} || true
+cd /usr/src/rockbox/build
 
 # FullZip for Sansa Clip+
 [[ -f Makefile ]] || /usr/src/rockbox/rockbox/tools/configure --target=62 --type=N
-make clean && make -j && make fullzip
+make -j && make fullzip
 mv rockbox-full.zip /usr/src/rockbox/artifacts/sansaclipp-rockbox-full.zip
