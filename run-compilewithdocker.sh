b6e280a1dbaee38db0a013e73f9f9c23a62b037d
Eugene <e@egeek.me>
2025-01-17 18:12:37 +0200

    Clarify where artifacts are created

--- run-compilewithdocker.sh
+++ run-compilewithdocker.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 SRC_FOLDER_PATH=$(realpath rockbox-git)
 #docker run -it --rm -v "$SRC_FOLDER_PATH":/usr/src/rockbox --name rockboxgetbuilds rockbox /bin/bash
 docker run --rm -v "$SRC_FOLDER_PATH":/usr/src/rockbox --name rockboxgetbuilds rockbox /usr/src/rockbox/create-builds.sh
-echo "Script ended, enjoy your artifacts"
+echo "Script ended, enjoy your artifacts in rockbox-git/artifacts/"
